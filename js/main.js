aa5a1166205dbdaae851861d71c7911a16a5123f
ocheretnyi1207 <anton@ocheretnyi.ru>
2019-09-26 20:20:39 +0300

    Генерирует моковые данные и объекты

--- js/main.js
+++ js/main.js
@@ -2,22 +2,23 @@
 
 // Mohs data
 var NUMBER_ADS = 8; // количество похожих объявлений
-var numberAvatars = ['01', '02', '03', '04', '05', '06', '07', '08'];
+
+var indexAvatars = ['01', '02', '03', '04', '05', '06', '07', '08'];
 var titleAds = ['1 объявление', '2 объявление', '3 объявление', '4 объявление', '5 объявление', '6 объявление', '7 объявление', '8 объявление'];
 var houseAdress = ['Лиговский пр-кт', 'Растанная ул.', 'Курляндская ул', 'Будапештская ул.', 'Средний проспект ВО', 'ул. Белинского', 'Литейный пр', 'ул. Маяковского'];
-var houseCosts = [1250, 2845, 5000, 12384, 10200, 50000, 35000, 28000];
+var housePrice = [1250, 2845, 5000, 12384, 10200, 50000, 35000, 28000];
 var houseTypes = ['palace', 'flat', 'house', 'bungalo'];
 var houseNumberRooms = [1, 2, 3, 4, 5];
 var houseNumberGuests = [1, 2, 3, 4, 5];
-var timeEntrys = ['12:00', '13:00', '14:00'];
-var timeDepartures = ['12:00', '13:00', '14:00'];
+var timeCheckins = ['12:00', '13:00', '14:00'];
+var timeCheckouts = ['12:00', '13:00', '14:00'];
 var houseFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var houseDefinitions = ['Качественный ремонт', 'Около метро', 'Тихие соседи', 'Рядом магазины', 'Красивый вид из окон', 'Много детских площадок', 'Недалеко парки'];
-var houseImage = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var houseDescriptions = ['Качественный ремонт', 'Около метро', 'Тихие соседи', 'Рядом магазины', 'Красивый вид из окон', 'Много детских площадок', 'Недалеко парки'];
+var housePhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var coordinateX = [250, 300, 350, 400, 450, 500, 550, 600];
 var coordinateY = [150, 200, 250, 300, 350, 400, 450, 500];
 
-// Функция генерации рандомного индекса элемента массива
+// Функция получения рандомного индекса элемента массива
 
 var getRandomIndexElement = function (arr) {
   var minIndex = 0;
@@ -26,83 +27,39 @@ var getRandomIndexElement = function (arr) {
   return randomIndex;
 };
 
-// Функция генерации рандомного элемента массива
+// Функция получения рандомного элемента массива
 
 var getRandomArrayElement = function (arr) {
   var randomIndex = getRandomIndexElement(arr);
   return arr[randomIndex];
 };
 
-var indexAvatars = [];
-var houseClasses = [];
-var houseRooms = [];
-var houseGuests = [];
-var timeCheckins = [];
-var timeCheckouts = [];
-var houseAmeneties = [];
-var houseDescription = [];
-var housePhotos = [];
-var locationX = [];
-var locationY = [];
 
 var similarAds = [];
 
 for (var i = 0; i < NUMBER_ADS; i++) {
 
-  indexAvatars[i] = getRandomArrayElement(numberAvatars);
-  indexAvatars.push(indexAvatars[i]);
-
-  houseClasses[i] = getRandomArrayElement(houseTypes);
-  houseClasses.push(houseClasses[i]);
-
-  houseRooms[i] = getRandomArrayElement(houseNumberRooms);
-  houseRooms.push(houseRooms[i]);
-
-  houseGuests[i] = getRandomArrayElement(houseNumberGuests);
-  houseGuests.push(houseGuests[i]);
-
-  timeCheckins[i] = getRandomArrayElement(timeEntrys);
-  timeCheckins.push(timeCheckins[i]);
-
-  timeCheckouts[i] = getRandomArrayElement(timeDepartures);
-  timeCheckouts.push(timeCheckouts[i]);
-
-  houseAmeneties[i] = getRandomArrayElement(houseFeatures);
-  houseAmeneties.push(houseAmeneties[i]);
-
-  houseDescription[i] = getRandomArrayElement(houseDefinitions);
-  houseDescription.push(houseDescription[i]);
-
-  housePhotos[i] = getRandomArrayElement(houseImage);
-  housePhotos.push(housePhotos[i]);
-
-  locationX[i] = getRandomArrayElement(coordinateX);
-  locationX.push(locationX[i]);
-
-  locationY[i] = getRandomArrayElement(coordinateY);
-  locationY.push([i]);
-
   similarAds[i] = {
     author: {
-      avatar: 'img/avatars/user' + (indexAvatars[i]).toString(10) + '.png',
+      avatar: 'img/avatars/user' + getRandomArrayElement(indexAvatars).toString(10) + '.png',
     },
 
-    address: houseAdress[i],
     offer: {
       title: titleAds[i],
       address: houseAdress[i],
-      price: houseCosts[i],
-      type: houseClasses[i],
-      rooms: houseNumberRooms[i],
-      guests: houseNumberGuests[i],
-      checkin: timeCheckins[i],
-      checkout: timeCheckouts[i],
-      features: houseAmeneties[i],
-      description: houseDescription[i],
-      photos: housePhotos[i],
+      price: housePrice[i],
+      type: getRandomArrayElement(houseTypes),
+      rooms: getRandomArrayElement(houseNumberRooms),
+      guests: getRandomArrayElement(houseNumberGuests),
+      checkin: getRandomArrayElement(timeCheckins),
+      checkout: getRandomArrayElement(timeCheckouts),
+      features: getRandomArrayElement(houseFeatures),
+      description: houseDescriptions[i],
+      photos: getRandomArrayElement(housePhotos),
+
       location: {
-        x: locationX[i],
-        y: locationY[i],
+        x: coordinateX[i],
+        y: coordinateY[i],
       }
     }
   };
@@ -115,16 +72,18 @@ document.querySelector('.map').classList.remove('map--faded');
 
 // Ищем шаблон и место для вставки шаблонов
 
-var similarAdsTemplate = document.querySelector('#pin').content.querySelector('.map__pin'); // шаблон метки
+var similarTemplate = document.querySelector('#pin').content.querySelector('.map__pin'); // шаблон метки
 var similarList = document.querySelector('.map'); // вставляем шаблоны на карту
 
 
-// Функция отрисовки объявления
+// Функция отрисовки указателя
 
-var renderAds = function () {
-  var adsElement = similarAdsTemplate.cloneNode(true);
-  adsElement.style = 'left:' + (similarAds[i].location.x).toString(10) + 'px' + ';' + 'top;' + (similarAds[i].location.y).toString(10) + 'px' + ';';
-  adsElement.querySelector('img').src = similarAds[i].avatar;
+var renderTemplate = function () {
+  var templateElement = similarTemplate.cloneNode(true);
+  templateElement.style = similarAds[i].x + 'px';
+  templateElement.style = similarAds[i].y + 'px';
+  templateElement.querySelector('img').src = similarAds[i].avatar;
+  templateElement.querySelector('img').alt = similarAds[i].title;
 };
 
 // Вставка шаблонов на карту
@@ -132,7 +91,7 @@ var renderAds = function () {
 var fragment = document.createDocumentFragment();
 
 for (i = 0; i < similarAds.length; i++) {
-  fragment.appendChild(renderAds(similarAds[i]));
+  fragment.appendChild(renderTemplate(similarAds[i]));
 }
 
 similarList.appendChild(fragment);
