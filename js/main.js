046f6763462754581727568aa68f7abb6d9ef505
ocheretny1207 <anton@ocheretnyi.ru>
2019-09-28 17:01:30 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// Mohs data
+// Входные данные
 var NUMBER_ADS = 8; // количество похожих объявлений
 
 var indexAvatars = ['01', '02', '03', '04', '05', '06', '07', '08'];
 var titleAds = ['1 объявление', '2 объявление', '3 объявление', '4 объявление', '5 объявление', '6 объявление', '7 объявление', '8 объявление'];
-var houseAdress = ['Лиговский пр-кт', 'Растанная ул.', 'Курляндская ул', 'Будапештская ул.', 'Средний проспект ВО', 'ул. Белинского', 'Литейный пр', 'ул. Маяковского'];
+var houseAddress = ['Лиговский пр-кт', 'Растанная ул.', 'Курляндская ул', 'Будапештская ул.', 'Средний проспект ВО', 'ул. Белинского', 'Литейный пр', 'ул. Маяковского'];
 var housePrice = [1250, 2845, 5000, 12384, 10200, 50000, 35000, 28000];
 var houseTypes = ['palace', 'flat', 'house', 'bungalo'];
 var houseNumberRooms = [1, 2, 3, 4, 5];
@@ -45,7 +45,7 @@ for (var i = 0; i < NUMBER_ADS; i++) {
 
     offer: {
       title: titleAds[i],
-      address: houseAdress[i],
+      address: houseAddress[i],
       price: housePrice[i],
       type: getRandomArrayElement(houseTypes),
       rooms: getRandomArrayElement(houseNumberRooms),
@@ -75,6 +75,11 @@ var similarMapPinTemplate = document.querySelector('#pin').content.querySelector
 // Место для вставки шаблона
 var similarMapPinList = document.querySelector('.map');
 
+// Шаблон модального окна с информацией об объявлении
+var similarCardTemplate = document.querySelector('#card').content.querySelector('.map-card');
+
+var similarCardList = document.querySelector('.map__pins');
+
 // Функция отрисовки map__pin
 var renderMapPinTemplate = function () {
   var templateMapPinElement = similarMapPinTemplate.cloneNode(true);
@@ -86,11 +91,28 @@ var renderMapPinTemplate = function () {
   return templateMapPinElement;
 };
 
-// Вставка шаблонов map__pin на карту
+// Функция отрисовки модального окна с информацией об объявлении
+var renderCardTemplate = function () {
+  var templateCardElement = similarCardTemplate.cloneNode(true);
+  templateCardElement.querySelector('.popup__title').textContent = similarAds[i].title;
+  templateCardElement.querySelector('.popup__text--address').textContent = similarAds[i].address;
+  templateCardElement.querySelector('.popup__text--price').textContent = (similarAds[i].price).toString(10) + 'Р/ночь';
+  templateCardElement.querySelector('.popup__type').textContent = similarAds[i].type;
+  templateCardElement.querySelector('.popup__text--capacity').textContent = (similarAds[i].rooms).toString(10) + 'комнаты для' + (similarAds[i].guests).toString(10);
+  templateCardElement.querySelector('.popup__text--time').textContent = 'Заезд после' + (similarAds[i].checkin).toString(10) + ',' + 'выезд до' + (similarAds[i].checkout).toString(10);
+  templateCardElement.querySelector('.popup__features').textcontent = similarAds[i].features;
+  templateCardElement.querySelector('.popup__photos').src = similarAds[i].photos;
+  templateCardElement.querySelector('.popup__avatar').src = similarAds[i].avatar;
+  return templateCardElement;
+};
+
+// Вставка шаблонов на карту
 var fragment = document.createDocumentFragment();
 
 for (i = 0; i < similarAds.length; i++) {
   fragment.appendChild(renderMapPinTemplate(similarAds[i]));
+  fragment.appendChild(renderCardTemplate(similarAds[i]));
 }
 
 similarMapPinList.appendChild(fragment);
+similarCardList.appendChild(fragment);
