fc9a59fb5c27595f392b6a96e5013ff21efdb35b
ocheretnyi1207 <anton@ocheretnyi.ru>
2019-11-09 23:41:15 +0300

    bug fixed render.js

--- js/backend.js
+++ js/backend.js
@@ -1,31 +1,41 @@
 'use strict';
 
 (function () {
-  var createXhr = function (method, takeUrl, successCallback, errorCallback) {
+
+  var createXhr = function (method, url, successCallback, errorCallback) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
+    xhr.open(method, url);
 
-    xhr.open(method, takeUrl);
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === window.util.SERVER_RESPONCE_SUCCESS) {
-        successCallback('Данные были успешно отправлены');
-      } else {
-        errorCallback('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-
-
-      xhr.addEventListener('error', function () {
-        errorCallback('Произошла ошибка соединения');
+    if (url === window.util.URL_LOAD) {
+      xhr.addEventListener('load', function () {
+        if (xhr.status === window.util.SERVER_RESPONCE_SUCCESS) {
+          successCallback(xhr.response);
+        } else {
+          errorCallback('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
       });
+    }
 
-
-      xhr.addEventListener('timeout', function () {
-        errorCallback('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    if (url === window.util.URL_UPLOAD) {
+      xhr.addEventListener('load', function () {
+        if (xhr.status === window.util.SERVER_RESPONCE_SUCCESS) {
+          successCallback('Выше объявление было успешно размещено!');
+        } else {
+          errorCallback('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
       });
+    }
 
-      xhr.timeout = window.util.TIMEOUT;
+    xhr.addEventListener('error', function () {
+      errorCallback('Произошла ошибка соединения');
     });
+
+    xhr.addEventListener('timeout', function () {
+      errorCallback('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = window.util.TIMEOUT;
 
     return xhr;
   };
@@ -37,7 +47,6 @@
   var upload = function (data, successCallback, errorCallback) {
     createXhr('POST', window.util.URL_UPLOAD, successCallback, errorCallback).send(data);
   };
-
 
   window.backend = {
     load: load,
